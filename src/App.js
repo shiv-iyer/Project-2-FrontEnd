6f681460e1b5041bfdb535bb9deca276d75e8483
Shiv <shiviyer55x@gmail.com>
2023-03-25 14:35:00 +0800

    basic component testing

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// import components
+import DisplayPost from "./components/DisplayPost";
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 
 function App() {
   return (
     // enclose everything within one React.Fragment
     <React.Fragment>
       {/* Create the NavBar for the homepage, expand in full size at the md breakpoint and onwards */}
-      <Navbar bg="light" expand="md">
+      <Navbar expand="md" id="main-navbar">
         <Container>
           {/* Navbar.Brand is the main element in the navbar */}
-          <Navbar.Brand href="#home">Royale Raves</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="35"
+              height="35"
+              className="d-inline-block align-center"
+              alt="RoyaleRaves logo"
+            />
+            Royale Raves</Navbar.Brand>
           {/* Navbar.Toggle creates the 'hamburger menu', controls the collapsible Navbar */}
           <Navbar.Toggle aria-controls="home-navbar"/>
           <Navbar.Collapse id="home-navbar">
@@ -28,8 +39,11 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <DisplayPost/>
       <div className="App">
         <header className="App-header">
+          <p>Welcome to RoyaleRaves, your #1 forum for Clash Royale decks!</p>
+          <p>To get started, head over to the posts page to view all of our current posts, or alternatively, create one of your own.</p>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/DisplayPost.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// create the component: extend React.Component to have all the functionality of a react component
+export default class DisplayPost extends React.Component {
+    render(){
+        return <React.Fragment>
+            <h1>Testing.</h1>
+        </React.Fragment>
+    }
+}
