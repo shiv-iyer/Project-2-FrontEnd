642dede3b9860dd25c74600c5c6a7bc98c72e816
Shiv <shiviyer55x@gmail.com>
2023-03-26 20:56:50 +0800

    began work on home page with styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+// react-bootstrap components
+
+// stylesheet
+import "../pages.css";
+
 export default class Home extends React.Component {
     // state
     state = {
@@ -12,6 +17,15 @@ export default class Home extends React.Component {
         return (
             <React.Fragment>
                 This is the home page.
+                {/* Hero image container*/}
+                <div id="heroContainer" className="mb-3">
+                    <div id="heroContent">
+                        <h1>Welcome to Royale Raves!</h1>
+                    </div>
+                </div>
+                <div>
+                    <p>Discover the latest and most popular user-submitted decks in Clash Royale, and find out what everyone is saying about them!</p>
+                </div> 
             </React.Fragment>
         );
     }
