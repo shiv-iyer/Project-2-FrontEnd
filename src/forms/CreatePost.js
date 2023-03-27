3b5e242643d2a98c8fe0eeaec8e08c1e607decab
Shiv <shiviyer55x@gmail.com>
2023-03-27 15:01:23 +0800

    functional range slider for difficult and rating

--- src/forms/CreatePost.js
+++ src/forms/CreatePost.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import RangeSlider from 'react-bootstrap-range-slider';
 
 // stylesheet
 import "../pages.css";
@@ -10,8 +11,23 @@ export default class CreatePost extends React.Component {
 
     // state
     state = {
-
+        rating: 1,
+        difficulty: 1
     }
+
+    // functions
+    updateRating = (event) => {
+        this.setState({
+            rating: event.target.value
+        })
+    }
+
+    updateDifficulty = (event) => {
+        this.setState({
+            difficulty: event.target.value
+        });
+    };
+
 
     render(){
         return <React.Fragment>
@@ -19,18 +35,21 @@ export default class CreatePost extends React.Component {
             <div className="container post-form">
 
                 <h3>Main Post Info</h3>
-                <div className="mb-3 post-form-group">
-                    <Form.Label htmlFor="inputName">Post Name</Form.Label>
-                    {/* type=text for plain text; aria-describedby references what describes the form*/}
-                    {/* onChange should be here. */}
-                    <Form.Control
-                        type="text"
-                        id="inputName"
-                        aria-describedby="postNameHelp"
-                    />
-                    <Form.Text id="postNameHelp" muted>
-                        The name of your post — try to give your deck a descriptive title!
-                    </Form.Text>
+                <div className="post-form-group">
+                    <Form>
+                        <Form.Group controlId="inputName">
+                            <Form.Label>Post Name</Form.Label>
+                            {/* type=text for plain text; aria-describedby references what describes the form*/}
+                            {/* onChange should be here. */}
+                            <Form.Control
+                                type="text"
+                                aria-describedby="postNameHelp"
+                            />
+                            <Form.Text id="postNameHelp" muted>
+                                The name of your post — try to give your deck a descriptive title!
+                            </Form.Text>
+                        </Form.Group>
+                    </Form>
                 </div>
 
                 <div className="mb-3 post-form-group">
@@ -39,20 +58,54 @@ export default class CreatePost extends React.Component {
                 </div>
 
                 <h3>Deck Info</h3>
-                <div className="mb-3 post-form-group">
-                    <Form.Label htmlFor="inputOverview">Deck Overview</Form.Label>
-                    {/* type=text for plain text; aria-describedby references what describes the form*/}
-                    <Form.Control
-                        type="text"
-                        id="inputOverview"
-                        aria-describedby="overviewHelp"
-                    />
-                    <Form.Text id="overviewHelp" muted>
-                        The overview of your deck — how would you describe it? What do you like about it? (Max x characters.)
-                    </Form.Text>
+                <div className="post-form-group">
+                    <Form>
+                        <Form.Group className="mb-2" controlId="inputOverview">
+                            <Form.Label>Deck Overview</Form.Label>
+                            {/* type=text for plain text; aria-describedby references what describes the form*/}
+                            <Form.Control
+                                type="text"
+                                aria-describedby="overviewHelp"
+                            />
+                            <Form.Text id="overviewHelp" muted>
+                                The overview of your deck — how would you describe it? What do you like about it? (Max x characters.)
+                            </Form.Text>
+                        </Form.Group>
+                    </Form>
 
                     {/* By default, Form.Text is an 'input', but can customize to TextArea if needed! */}
-                    <Form.Label>Strategy</Form.Label>
+                    <Form>
+                        <Form.Group className="mb-2" controlId="inputStrategy">
+                            <Form.Label>Strategy</Form.Label>
+                            <Form.Control
+                                type="text"
+                                as="textarea"
+                                rows={3}
+                            />
+                        </Form.Group>
+                        {/* Range slider for rating & difficulty! Works, can implement with a hook but use state for now */}
+                        {/* Seems like I can't really customize it well, maybe leave default for now and look into it later. */}
+                        <div className="slider-group">
+                            <row>
+
+                            </row>
+                            <h5>Rating</h5>
+                            <RangeSlider
+                                min={1}
+                                max={10}
+                                value={this.state.rating}
+                                onChange={this.updateRating}
+                            />
+                            <h5>Difficulty</h5>
+                            <RangeSlider
+                                min={1}
+                                max={5}
+                                value={this.state.difficulty}
+                                onChange={this.updateDifficulty}
+                            />
+                        </div>
+                    </Form>
+                
                 </div>
 
                 <div>
