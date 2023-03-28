acc88ebaf3b9b505fbeeba711edbbdf279ff481d
Shiv <shiviyer55x@gmail.com>
2023-03-28 14:05:23 +0800

    added base Express api url file

---
+++ src/components/BaseApi.js
@@ -0,0 +1,7 @@
+/* Run the Express app first, hosting the server on port 3000 */
+
+const BASE_API = "http://localhost:3000/"
+
+/* export to the other files, then pass in to whatever components you need */
+
+export default BASE_API;
