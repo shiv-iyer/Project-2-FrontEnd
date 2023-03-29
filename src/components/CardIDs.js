f9cebbe94ec24050d7e079f4c8fc949c75039b33
Shiv <shiviyer55x@gmail.com>
2023-03-29 16:28:57 +0800

    updated card ids

---
+++ src/components/CardIDs.js
@@ -0,0 +1,16 @@
+// save all of the cards' document IDs; this way, they can be referred to for the POST request
+
+export const cardIDs = {
+    xbow: "6412c055632f110d0e8812d0",
+    tesla: "6412c159632f110d0e8ba04d",
+    archers: "6412c19c632f110d0e8c7c19",
+    log: "6412c1ff632f110d0e8dcc71",
+    ice_Spirit: "641d4b9d04f85304f52ba96c",
+    skeletons: "641d4c4a04f85304f52ba96d",
+    fireball: "641d4fec04f85304f52ba96e",
+    knight: "641d507504f85304f52ba96f",
+    musketeer: "641d5c696bedf92c58be2d8c",
+    rocket: "641d5cd86bedf92c58be2d8d",
+    zap: "641d5d446bedf92c58be2d8e",
+    cannon: "641d5e176bedf92c58be2d8f"
+}
