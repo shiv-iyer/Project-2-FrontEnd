2329cf7ddc4287e4603fd6f26c1638e16f733991
Shiv <shiviyer55x@gmail.com>
2023-04-02 10:16:56 +0800

    added new empty string validation rule

--- src/components/validation.js
+++ src/components/validation.js
@@ -5,13 +5,21 @@
 export const validateName = (name) => {
     if (!name)
         return "Please enter a name!"; 
+    // if it's only whitespace; using regex (logic: replace all white space characters with an empty character, if resulting length
+    // if 0, it means the only characters in the string were regex)
+    if (!name.replace(/\s/g, '').length)
+        return "Please don't leave the name blank!";
     if (name.length > 50)
         return "Your name is too long; please enter a shorter name!";
+    if (name.length < 7)
+        return "Your name is too short; please enter a longer name!";
 }
 
 export const validateOverview = (overview) => {
     if (!overview)
         return "Please enter an overview!"
+    if (!overview.replace(/\s/g, '').length)
+        return "Please don't leave the overview blank!";
     if (overview.length < 30)
         return "Your overview is too short; please enter a longer description of the deck!";
 }
@@ -19,6 +27,8 @@ export const validateOverview = (overview) => {
 export const validateStrategy = (strategy) => {
     if (!strategy)
         return "Please enter a strategy!"
+    if (!strategy.replace(/\s/g, '').length)
+        return "Please don't leave the strategy blank!";
     if (strategy.length < 50)
         return "Your strategy is too short; please enter a longer tactical description of the deck and its playstyle!";
 }
