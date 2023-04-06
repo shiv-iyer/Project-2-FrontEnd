51758072b850e0d111097759e5630d46da9bf018
Shiv <shiviyer55x@gmail.com>
2023-04-06 14:51:38 +0800

    added functional search by archetype

--- src/pages/ViewPosts.js
+++ src/pages/ViewPosts.js
@@ -125,7 +125,7 @@ export default class ViewPosts extends React.Component {
         // get from API 
         // backtick strings `` and $ are for passing in a parameter as part of the string (axios.get requires a string?)
         // posts: the name of my collection I want to retrieve from, in this case it's posts
-        const postsResponse = await axios.get(`${BASE_API}posts?name=${this.state.search}`);
+        const postsResponse = await axios.get(`${BASE_API}posts?name=${this.state.search}&archetype=${this.state.archetypeFilter}`);
 
         // set state from the response
         this.setState({
