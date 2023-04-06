17d8ac63c5f2cfc20f1358a8da7b095e69905fc1
Shiv <shiviyer55x@gmail.com>
2023-04-06 14:59:45 +0800

    styling, bouncy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,23 +2,25 @@
 import React from 'react';
 import {Form, Button, Row, Col} from "react-bootstrap";
 
+import '../assets/componentStyles.css';
+
 // let's try a function-based component
 
 export default function SearchBar(props){
     return (
         <React.Fragment>
-            <Row className="my-4 mx-4">
+            <Row className="my-4 mx-4 searchBarContainer">
                 <Col>
                     <Form.Control
                     type="text"
                     value={props.search}
                     onChange={props.handleSearchChange}
                     placeholder="Search posts..."
-                    className="rounded-pill"
+                    className="rounded-pill searchInput bouncy"
                     />
                 </Col>
                 <Col xs="auto">
-                    <Form.Select onChange={props.handleFilterChange}>
+                    <Form.Select onChange={props.handleFilterChange} className="filterSelect bouncy">
                         <option value="">Filter by Archetype</option>
                         <option value="Beatdown">Beatdown</option>
                         <option value="Control">Control</option>
@@ -30,7 +32,7 @@ export default function SearchBar(props){
                     </Form.Select>
                 </Col>
                 <Col xs="auto">
-                    <Button onClick={props.findResults}>Find Results</Button>
+                    <Button onClick={props.findResults} className="bouncy">Find Results</Button>
                 </Col>
             </Row>
         </React.Fragment>
