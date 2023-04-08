87a1c7761f85798779b517b82df01d61b76f4bc8
Shiv <shiviyer55x@gmail.com>
2023-04-08 11:31:27 +0800

    cleaned up a lot of code / comments, updated to map the post deck

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,52 +24,32 @@ export default class Post extends React.Component {
 
         return <React.Fragment>
             <div className="border shadow">
-                                    <h1 className="p-3">Post Name: {this.props.post.name}</h1>
-                                    <h5 className="p-1">Posted by: {this.props.post.userThatPosted}</h5>
-                                    <h5 className="p-1">Date posted: {this.props.post.dateOfCreation}</h5>
-                                    <h6 className="p-1">Date updated: {this.props.post.dateOfUpdation}</h6>
-                                    <div id="deckContainer" className="p-3 mb-2">
-                                        <h5 className="my-2">Deck</h5>
-                                        {this.props.post.deck.cards.map((card) => (
-                                            <div>
-                                                <p>{card.cardName}</p>
-                                                <img src={card.cardURL} alt="One card in the user's deck of 8 cards"/>
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <p className="p-3 mb-2">Overview: {this.props.post.postInfo.overview}</p>
-                                    <p className="p-3 mb-2">Strategy: {this.props.post.postInfo.strategy}</p>
-                                    <p className="p-3 mb-2">Archetype: {this.props.post.archetype}</p>
-                                    <p className="p-1">Rating: {this.props.post.postInfo.rating}</p>
-                                    <p className="p-1">Difficulty: {this.props.post.postInfo.difficultyLevel}</p>
-                                    <Button
-                                        variant="secondary"
-                                        onClick={()=> this.props.updatePost(this.props.post)}
-                                        >Edit</Button>
-                                </div>
-            {/* 
-            <Card className="post-card mb-3 p-2">
-                <Card.Title>— Post Name —</Card.Title>
-                <Card.Subtitle className="mb-1 text-muted subtitle">by @username</Card.Subtitle>
-                <Card.Subtitle className="mb-3 text-muted subtitle">2023-03-26</Card.Subtitle>
-                // Probably need to put the deck here. As well as deck information, maybe deck and card components
-                <Card.Text className="headers mb-1">Overview</Card.Text>
-                <Card.Text className="mb-3">
-                    3.0 Xbow Cycle is hfbjdcv hd jnxkmlzjnb jnzkbjfdhknvclxznbhjc lorem ipsum mwhatever.
-                </Card.Text>
-                <Card.Text className="headers mb-1">Strategy</Card.Text>
-                <Card.Text className="mb-3">
-                    Just anyhow throw cards lol!
-                </Card.Text>
-                <Container>
-                    <Row>
-                        <Col><Card.Text className="side-headers">Rating: {this.state.rating}</Card.Text></Col>
-                        <Col><Card.Text className="side-headers">Difficulty: {this.state.difficulty}</Card.Text></Col>
-                    </Row>
-                </Container>
-            </Card>
-            */}
-
+                <h1 className="p-3">Post Name: {this.props.post.name}</h1>
+                <h5 className="p-1">Posted by: {this.props.post.userThatPosted}</h5>
+                <h5 className="p-1">Date posted: {this.props.post.dateOfCreation}</h5>
+                <h6 className="p-1">Date updated: {this.props.post.dateOfUpdation}</h6>
+                <div className="deckContainer">
+                    {this.props.post.deck.cards.map((card) => (
+                        <div key={card.cardID} className="cardItem large">
+                            <img
+                                src={card.cardURL}
+                                alt="One card in the user's deck of 8 cards"
+                                style={{ maxWidth: "100%" }}
+                            />
+                            <p>{card.cardName}</p>
+                        </div>
+                    ))}
+                </div>
+                <p className="p-3 mb-2">Overview: {this.props.post.postInfo.overview}</p>
+                <p className="p-3 mb-2">Strategy: {this.props.post.postInfo.strategy}</p>
+                <p className="p-3 mb-2">Archetype: {this.props.post.archetype}</p>
+                <p className="p-1">Rating: {this.props.post.postInfo.rating}</p>
+                <p className="p-1">Difficulty: {this.props.post.postInfo.difficultyLevel}</p>
+                <Button
+                    variant="secondary"
+                    onClick={()=> this.props.updatePost(this.props.post)}
+                    >Edit</Button>
+            </div>
         </React.Fragment>
     }
 }
