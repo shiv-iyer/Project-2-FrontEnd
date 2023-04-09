595d0506a3662bf6e732527724795dab0cb7d23e
Shiv <shiviyer55x@gmail.com>
2023-04-09 14:11:52 +0800

    changed to base api

--- src/components/BaseApi.js
+++ src/components/BaseApi.js
@@ -1,6 +1,8 @@
 /* Run the Express app first, hosting the server on port 3000 */
 
-const BASE_API = "http://localhost:3000/"
+/* OLD API: http://localhost:3000/ */
+
+const BASE_API = "https://dwad22-project2-backend.onrender.com/"
 
 /* export to the other files, then pass in to whatever components you need */
 
