fb80b1dd4e5f0ac89e0b2e7df200eefc053bb9d5
Shiv <shiviyer55x@gmail.com>
2023-04-09 16:51:55 +0800

    card grid for card page

---
+++ src/components/CardGrid.js
@@ -0,0 +1,31 @@
+// imports
+import React from 'react';
+import {Form, Button, Row, Col, Container} from "react-bootstrap";
+
+import '../assets/componentStyles.css';
+
+// let's try a function-based component
+
+export default function CardGrid(props){
+    return (        
+        <React.Fragment>
+            <div>
+                    {/* wrap everything in a parent div, that way it can be formatted and ordered nicely. */}
+                    <div className="cardGrid">
+                        {/* map the labels object, creating a clickable card based on the card name and its corresponding image URL */}
+                        {/* we avoid hardcoding by mapping everything in this.state.labels. */}
+                        {props.cards.map((card, index) => {
+                            return (
+                                <div key={index} className="clashCard customClashCard larger"
+                                 onClick={() => props.click(card)}>
+                                    <img src={card.cardURL} alt={card.cardName} className="cardImg"/>
+                                     <p>{card.cardName}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+            </div>
+        </React.Fragment>
+
+    );
+}
