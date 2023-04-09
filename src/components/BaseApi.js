96e3eefa9e00f188f177533560a1bad212d44f5e
Shiv <shiviyer55x@gmail.com>
2023-04-09 16:51:13 +0800

    switched to local for testing

--- src/components/BaseApi.js
+++ src/components/BaseApi.js
@@ -2,7 +2,8 @@
 
 /* OLD API: http://localhost:3000/ */
 
-const BASE_API = "https://dwad22-project2-backend.onrender.com/"
+// const BASE_API = "https://dwad22-project2-backend.onrender.com/"
+const BASE_API = "http://localhost:3000/";
 
 /* export to the other files, then pass in to whatever components you need */
 
