db0525e0585cb7035b34628df3dac6e62b14f962
Shiv <shiviyer55x@gmail.com>
2023-04-17 21:27:50 +0800

    this...

--- src/forms/CreatePost.js
+++ src/forms/CreatePost.js
@@ -189,7 +189,7 @@ export default class CreatePost extends React.Component {
             alert("Please select all 8 cards!");
         } else {
             // move it above?
-            clearPostInfo();
+            this.clearPostInfo();
             // maybe think of a way to improve alert later
             alert("Deck has been submitted!");
             // axios.post has two parameters; the api URL, and the request body.
